1b59f0acc1d9c3dbe0f4cf1766a721b2083d133f
nipunraj <nipunraj99@gmail.com>
2023-09-24 21:22:22 +0530

    reo

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 document.getElementById("showVideo").addEventListener("click", function () {
   var videoPopup = document.getElementById("videoPopup");
-  var youtubeIframe = document.getElementById("youtubeIframe");
 
-  // Set the YouTube video URL
-  var videoURL =
+  // Create the YouTube iframe element
+  var youtubeIframe = document.createElement("iframe");
+  youtubeIframe.id = "youtubeIframe";
+  youtubeIframe.width = "560";
+  youtubeIframe.height = "315";
+  youtubeIframe.src =
     "https://www.youtube.com/watch?v=RpC85RO0okA&ab_channel=T-Series";
-  youtubeIframe.src = videoURL;
+  youtubeIframe.frameBorder = "0";
+  youtubeIframe.allowFullscreen = true;
 
-  // Show the popup
+  videoPopup.appendChild(youtubeIframe);
+
   videoPopup.style.display = "block";
 });
 
 document.getElementById("closePopup").addEventListener("click", function () {
   var videoPopup = document.getElementById("videoPopup");
   var youtubeIframe = document.getElementById("youtubeIframe");
-
-  // Clear the video URL
-  youtubeIframe.src = "";
+  videoPopup.removeChild(youtubeIframe);
 
   // Hide the popup
   videoPopup.style.display = "none";
