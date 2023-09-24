6180ec99c7a58fe96ec271b1f3f394cc9707ee30
nipunraj <nipunraj99@gmail.com>
2023-09-24 22:25:13 +0530

    yes

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
+// document.getElementById("showVideo").addEventListener("click", function () {
+//   var videoPopup = document.getElementById("videoPopup");
+
+//   // Create the YouTube iframe element
+//   var youtubeIframe = document.createElement("iframe");
+//   youtubeIframe.id = "youtubeIframe";
+//   youtubeIframe.width = "560";
+//   youtubeIframe.height = "315";
+//   youtubeIframe.src =
+//     "https://www.youtube.com/embed/RpC85RO0okA?si=eE_QDkqMi2HPv595";
+//   youtubeIframe.frameBorder = "0";
+//   youtubeIframe.allowFullscreen = true;
+
+//   videoPopup.appendChild(youtubeIframe);
+
+//   videoPopup.style.display = "block";
+// });
+
+// document.getElementById("closePopup").addEventListener("click", function () {
+//   var videoPopup = document.getElementById("videoPopup");
+//   var youtubeIframe = document.getElementById("youtubeIframe");
+//   videoPopup.removeChild(youtubeIframe);
+
+//   // Hide the popup
+//   videoPopup.style.display = "none";
+// });
+
 document.getElementById("showVideo").addEventListener("click", function () {
-  var videoPopup = document.getElementById("videoPopup");
+  var videoModal = document.getElementById("videoModal");
+  var youtubeIframe = document.getElementById("youtubeIframe");
 
-  // Create the YouTube iframe element
-  var youtubeIframe = document.createElement("iframe");
-  youtubeIframe.id = "youtubeIframe";
-  youtubeIframe.width = "560";
-  youtubeIframe.height = "315";
-  youtubeIframe.src =
+  // Set the YouTube video URL
+  var videoURL =
     "https://www.youtube.com/embed/RpC85RO0okA?si=eE_QDkqMi2HPv595";
-  youtubeIframe.frameBorder = "0";
-  youtubeIframe.allowFullscreen = true;
+  youtubeIframe.src = videoURL;
 
-  videoPopup.appendChild(youtubeIframe);
-
-  videoPopup.style.display = "block";
+  // Show the modal
+  videoModal.style.display = "block";
 });
 
-document.getElementById("closePopup").addEventListener("click", function () {
-  var videoPopup = document.getElementById("videoPopup");
+document.getElementById("closeModal").addEventListener("click", function () {
+  var videoModal = document.getElementById("videoModal");
   var youtubeIframe = document.getElementById("youtubeIframe");
-  videoPopup.removeChild(youtubeIframe);
 
-  // Hide the popup
-  videoPopup.style.display = "none";
+  // Clear the video URL
+  youtubeIframe.src = "";
+
+  // Hide the modal
+  videoModal.style.display = "none";
 });
